6d53adc07110fb9e0a72bdf1ed66857ae3f1f973
casey langen <casey.langen@gmail.com>
2022-12-29 12:41:27 -0800

    Fix high CPU usage.

--- wingui/pdckbd.c
+++ wingui/pdckbd.c
@@ -19,15 +19,9 @@ extern int PDC_key_queue[KEY_QUEUE_SIZE];
 
 bool PDC_check_key(void)
 {
-    static int count;
-
     if( PDC_key_queue_low != PDC_key_queue_high)
         return TRUE;
-    if( count++ == 100)
-    {
-        count = 0;
-        PDC_napms( 1);
-    }
+    PDC_napms( 1);
     return FALSE;
 }
 
